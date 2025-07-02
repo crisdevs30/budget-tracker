269d296ad9bcef24d24d6483f136294a0ad26316
crisdevs30 <crisdevs30@example.com>
2025-07-02 15:51:52 -0400

    Added PWA icon and manifest

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', e => {
+  console.log('Service Worker: Installed');
+  e.waitUntil(
+    caches.open('budget-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/script.js',
+        '/manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
+
